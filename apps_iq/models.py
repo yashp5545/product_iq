97c49fa2c13a2ca1def0df533728d0faf3873d65
automatefreely <automatefreely@gmail.com>
2024-05-05 02:50:34 +0530

    minor updates

--- apps_iq/models.py
+++ apps_iq/models.py
@@ -3,12 +3,13 @@ from django.contrib.postgres.fields import ArrayField
 from enum import Enum
 from users.models import User
 
+LEN_MAX = 400
 
 # Create your models here.
 class App(models.Model):
-    app_name = models.CharField(max_length=255)
+    app_name = models.CharField(max_length=LEN_MAX)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # price = models.DecimalField(max_digits=10, decimal_places=2)
     app_theme_color = models.CharField(max_length=50)
     # Assuming app logos are uploaded to a directory
     app_logo = models.ImageField(upload_to='app_logos/')
@@ -21,24 +22,24 @@ class App(models.Model):
 
 
 class Module(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     description = models.TextField()
     active = models.BooleanField(default=True)
     app = models.ForeignKey(App, on_delete=models.CASCADE)
 
     module_prompt = models.TextField(default='')
-    lebel_prompt = models.TextField(default='')
+    # lebel_prompt = models.TextField(default='')
     master_prompt = models.TextField(default='')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Product_coach_module"
+        verbose_name = "Product_Coach_Module"
 
 
 class Challenge(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     description = models.TextField()
     active = models.BooleanField(default=True)
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
@@ -47,11 +48,11 @@ class Challenge(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Product_coach_challenge"
+        verbose_name = "Product_Coach_Challenge"
 
 
 class Level(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     description = models.TextField(verbose_name="level_question")
     active = models.BooleanField(default=True)
     challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
@@ -63,7 +64,7 @@ class Level(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Product_coach_lebel"
+        verbose_name = "Product_Coach_Level"
 
 
 class LevelResponses(models.Model):
@@ -78,11 +79,11 @@ class LevelResponses(models.Model):
         return self.user.username + ' - ' + self.level.name
 
     class Meta:
-        verbose_name = "Product_coach_lebel_Response"
+        verbose_name = "Product_Coach_Level_Response"
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     description = models.TextField()
     active = models.BooleanField(default=True)
 
@@ -92,11 +93,11 @@ class Categories(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Product_worktools_Categorie"
+        verbose_name = "Product_Worktools_Categorie"
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     description = models.TextField()
     active = models.BooleanField(default=True)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
@@ -111,7 +112,7 @@ class Skill(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Product_worktools_Skill"
+        verbose_name = "Product_Worktools_Skill"
 
 
 class FromInputType(Enum):
@@ -128,7 +129,7 @@ class Question(models.Model):
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
 
     class Meta():
-        verbose_name = "Product_worktools_Question"
+        verbose_name = "Product_Worktools_Field"
 
 
 class SkillResponses(models.Model):
@@ -137,11 +138,11 @@ class SkillResponses(models.Model):
     answer = models.JSONField()
 
     class Meta:
-        verbose_name = " Product_worktools_Responce"
+        verbose_name = " Product_Worktools_Response"
 
 
 class Section(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     active = models.BooleanField(default=True)
     app = models.ForeignKey(App, on_delete=models.CASCADE)
 
@@ -153,7 +154,7 @@ class Section(models.Model):
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     active = models.BooleanField(default=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
 
@@ -161,11 +162,11 @@ class Topic(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Product_IQ_topic"
+        verbose_name = "Product_IQ_Topic"
 
 
 class Lession(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=LEN_MAX)
     description = models.TextField()
     active = models.BooleanField(default=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
@@ -174,4 +175,4 @@ class Lession(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Product_IQ_lession"
+        verbose_name = "Product_IQ_Lesson"
