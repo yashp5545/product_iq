5dfc6a74057aa2ac52ef268f94f1f728bfd92b47
automatefreely <automatefreely@gmail.com>
2024-05-20 15:37:49 +0530

    url update

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('auth/logout', views.logout, name="logout"),
     path('user/update', views.update_user, name="update_user"),
     path('user/add/referredby', views.add_referred_by, name='add_referred_by'),
-    path('user/password/forget/', include('django.contrib.auth.urls')),
+    path('user/action/', include('django.contrib.auth.urls')),
 ]
